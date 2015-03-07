4293728f65c9b653aff531749cd49235d706bce3
Julian Qian <julian@onlycoin.com>
2015-03-06 19:12:18 -0800

    RemoveDup

--- interview/src/linear/RemoveDupFromSort.java
+++ interview/src/linear/RemoveDupFromSort.java
@@ -1,19 +1,27 @@
 package linear;
 
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class RemoveDupFromSort {
-    public static void run(int[] a) {
-        
+    public static int[] run(int[] a) {
+        int pos = 0;
+        for (int i = 0; i<a.length; ++ i) {
+            if (a[pos] != a[i]) {
+                pos++;
+                a[pos] = a[i];
+            }
+        }
+        return Arrays.copyOf(a, pos+1);
     }
     @Test
     public void test() {
         int[] a = {1,1,2,2,3,5,7,8,8,8,8};
         int[] b = {1,2,3,5,7,8};
-        
-        Assert.assertArrayEquals(a, b);
+        Assert.assertArrayEquals(b, run(a));
         //Collections.sort(list);
     }
 
